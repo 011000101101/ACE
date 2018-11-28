270b18cedfd07c5ebd657f824f5e9e7e5256bf5a
Vincenz Mechler <011000101101@gmx.de>
2018-11-28 20:50:23 +0100

    fixing clock net error

--- task_1/src/designAnalyzer/inputParser/NetlistParser.java
+++ task_1/src/designAnalyzer/inputParser/NetlistParser.java
@@ -1,9 +1,6 @@
 package designAnalyzer.inputParser;
 
 import java.io.FileNotFoundException;
-import java.util.LinkedList;
-import java.util.List;
-
 import designAnalyzer.errorReporter.ErrorReporter;
 import designAnalyzer.structures.Net;
 import designAnalyzer.structures.StructureManager;
@@ -27,8 +24,6 @@ public class NetlistParser extends AbstractInputParser {
 	 * number of blocks already parsed
 	 */
 	private int numberOfBlocks=0;
-	
-	private List<String> clockNetNames= new LinkedList<String>();
 	
 	public NetlistParser(String newFilePath) throws FileNotFoundException {
 		super(newFilePath);
@@ -56,15 +51,8 @@ public class NetlistParser extends AbstractInputParser {
 			
 			case INPUT_TOKEN:
 				
-				if(!clockNetNames.contains(currentLine[1])) {
-					
-					currentBlock= parseInputBlock();
-					newBlock= true;
-					
-				}
-				else {
-					newBlock= false;
-				}
+				currentBlock= parseInputBlock();
+				newBlock= true;
 					
 				break;
 				
@@ -111,25 +99,25 @@ public class NetlistParser extends AbstractInputParser {
 	private NetlistBlock parseInputBlock() {
 		
 		NetlistBlock currentBlock= new IOBlock(currentLine[1], numberOfBlocks);
-			
-			currentLine= readLineAndTokenize();
-			
-			if(currentLine.length == 2) {	//check for correct number of arguments
-			
-				if(!"pinlist:".equals(currentLine[0])) { //check for correct token at start of line
-					ErrorReporter.reportSyntaxError("pinlist", currentLine[0], this);
-				}
-				
-				Net currentNet= structureManager.retrieveNet(currentLine[1], false);
-				currentBlock.connect(currentNet, 1); //connect specified net to output pin of input block
-				currentNet.setSource(currentBlock); //link block to net as only source
-				
-			}
-			else {
-				ErrorReporter.reportInvalidTokenCount(2, this);
+		
+		currentLine= readLineAndTokenize();
+		
+		if(currentLine.length == 2) {	//check for correct number of arguments
+		
+			if(!"pinlist:".equals(currentLine[0])) { //check for correct token at start of line
+				ErrorReporter.reportSyntaxError("pinlist", currentLine[0], this);
 			}
 			
-			return currentBlock;
+			Net currentNet= structureManager.retrieveNet(currentLine[1], false);
+			currentBlock.connect(currentNet, 1); //connect specified net to output pin of input block
+			currentNet.setSource(currentBlock); //link block to net as only source
+			
+		}
+		else {
+			ErrorReporter.reportInvalidTokenCount(2, this);
+		}
+		
+		return currentBlock;
 		
 	}
 	
@@ -215,12 +203,9 @@ public class NetlistParser extends AbstractInputParser {
 			//connect clock pin
 			if(!"open".equals(currentLine[6])) {	//ignore "open" pins
 				pinsConnected[5]= true;
-				/*
 				Net currentNet= structureManager.retrieveNet(currentLine[6], false);
 				currentBlock.connect(currentNet, 5);	//connect specified net to the clock pin of logic block
 				currentNet.setSource(currentBlock); //link block to net as only source
-				*/
-				structureManager.removeClockNet(currentLine[6]);
 			}
 			
 		}
@@ -267,13 +252,11 @@ public class NetlistParser extends AbstractInputParser {
 	}
 	
 	/**
-	 * deprecated: insert clock net into net collection if it does not already exist, else 
-	 * do nothing
+	 * insert clock net into net collection if it does not already exist, else do nothing
 	 */
 	private void parseGlobal() {
 		
-		//structureManager.retrieveNet(currentLine[1], true);
-		clockNetNames.add(currentLine[1]);
+		structureManager.retrieveNet(currentLine[1], true);
 		
 	}
 
