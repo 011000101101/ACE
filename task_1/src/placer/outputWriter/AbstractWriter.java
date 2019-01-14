a15b6a03c1cbdff61c6c88aa739e3fb851cc6648
Vincenz Mechler <vincenz.mechler@gmx.de>
2019-01-14 13:35:44 +0100

    implemented PlacementWriter and AbstractWriter and staged new files for commit...

---
+++ task_1/src/placer/outputWriter/AbstractWriter.java
@@ -0,0 +1,101 @@
+package placer.outputWriter;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import designAnalyzer.ParameterManager;
+import designAnalyzer.structures.StructureManager;
+
+public abstract class AbstractWriter {
+
+	/**
+	 * path to the file to be parsed
+	 */
+	protected String filePath;
+	
+	/**
+	 * file to be written
+	 */
+	protected File outputFile;
+	
+	/**
+	 * buffered writer for efficient file io
+	 */
+	protected BufferedWriter outputFileWriter;
+	
+	/**
+	 * object managing all datastructure instances <br>
+	 * -handling insertion, retrieval and others
+	 */
+	protected StructureManager structureManager;
+	
+	/**
+	 * object managing all structural parameters 
+	 */
+	protected ParameterManager parameterManager;
+
+	public AbstractWriter() {
+		
+		parameterManager= ParameterManager.getInstance();
+		
+	}
+	
+	/**
+	 * sets the new file path, creates a file and writes it
+	 * @param newFilePath the path for the file to be written
+	 */
+	public void write(String newFilePath) {
+		
+		filePath= newFilePath;
+		outputFile= new File(filePath);
+		try {
+			outputFileWriter= new BufferedWriter(new FileWriter(outputFile));
+		} catch (IOException e) {
+			System.err.println("Error occured trying to create a new file at path '" + filePath + "'");
+			e.printStackTrace();
+		}
+		writeAll();
+		
+	}
+	
+	/**
+	 * writes the complete file, block by block until end of file (indicated by null)
+	 */
+	private void writeAll() {
+		
+		try {
+			writeHeader();
+			writeAllBlocks();
+		} catch (IOException e1) {
+			System.err.println("Error occured trying to write to buffered writer");
+			e1.printStackTrace();
+		}
+		
+		
+		try {
+			outputFileWriter.close();
+		} catch (IOException e) {
+			System.err.println("Error occured trying to create a new file: unable to close bufferedWriter");
+			e.printStackTrace();
+		}
+		
+	}
+
+	/**
+	 * writes header of the file
+	 * @throws IOException 
+	 */
+	protected abstract void writeHeader() throws IOException;
+	
+	/**
+	 * writes all blocks of data into the output file
+	 */
+	protected abstract void writeAllBlocks() throws IOException;
+	
+	
+}
---
+++ task_1/src/placer/outputWriter/PlacementWriter.java
@@ -0,0 +1,47 @@
+package placer.outputWriter;
+
+import java.io.IOException;
+
+import designAnalyzer.structures.StructureManager;
+import designAnalyzer.structures.pathElements.blocks.NetlistBlock;
+
+public class PlacementWriter extends AbstractWriter {
+
+	private String referencedNetlistFileName;
+	private String referencedArchFileName;
+	
+	public PlacementWriter(String newReferencedNetlistFileName, String newReferencedArchFileName) {
+		super();
+		structureManager= StructureManager.getInstance();
+		referencedNetlistFileName= newReferencedNetlistFileName;
+		referencedArchFileName= newReferencedArchFileName;
+	}
+
+	@Override
+	protected void writeHeader() throws IOException {
+
+		outputFileWriter.write("Netlist file: " + referencedNetlistFileName + " Architecture file: " + referencedArchFileName);
+		outputFileWriter.write("\n" + "Array size: " + parameterManager.X_GRID_SIZE + " x " + parameterManager.Y_GRID_SIZE + " logic blocks");
+		outputFileWriter.write("\n");
+		
+	}
+
+	@Override
+	protected void writeAllBlocks() throws IOException {
+
+		for(NetlistBlock b : structureManager.getBlockMap().values()) {
+			writeOneBlock(b);
+		}
+
+	}
+
+	/**
+	 * writes one block of lines into the output file
+	 */
+	private void writeOneBlock(NetlistBlock b) throws IOException {
+
+		outputFileWriter.write("\n" + b.getName() + " " + b.getX() + " " + b.getY() + " " + (b.getSubblk_1() ? "1" : "0" ) + " #" + b.getBlockNumber() ); //no space between '#' and block number to be compatible with placement parser
+
+	}
+
+}
