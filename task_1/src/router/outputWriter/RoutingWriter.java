d8e29c74915e99e5511be449c5c26215a8ebc8b7
Rumei <rumei.ma@gmail.com>
2019-02-05 15:18:07 +0100

    routingWriter not implementing AbstractWriter naymore

--- task_1/src/router/outputWriter/RoutingWriter.java
+++ task_1/src/router/outputWriter/RoutingWriter.java
@@ -1,19 +1,96 @@
 package router.outputWriter;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collection;
 
+import designAnalyzer.ParameterManager;
 import designAnalyzer.structures.Net;
+import designAnalyzer.structures.StructureManager;
 import designAnalyzer.structures.pathElements.PathElement;
 import designAnalyzer.structures.pathElements.blocks.IOBlock;
 import designAnalyzer.structures.pathElements.blocks.LogicBlock;
 import designAnalyzer.structures.pathElements.blocks.NetlistBlock;
 import designAnalyzer.structures.pathElements.pins.OPin;
-import placer.outputWriter.AbstractWriter;
+import router.structures.tree.NodeOfResource;
 
-public class RoutingWriter extends AbstractWriter {
+public class RoutingWriter{
 
+	/**
+	 * path to the file to be parsed
+	 */
+	private String filePath;
+	
+	/**
+	 * file to be written
+	 */
+	private File outputFile;
+	
+	/**
+	 * buffered writer for efficient file io
+	 */
+	private BufferedWriter outputFileWriter;
+	
+	/**
+	 * object managing all datastructure instances <br>
+	 * -handling insertion, retrieval and others
+	 */
+	private StructureManager structureManager;
+	
+	/**
+	 * object managing all structural parameters 
+	 */
+	private ParameterManager parameterManager;
 
-	@Override
+	
+	public RoutingWriter() {
+		parameterManager= ParameterManager.getInstance();
+		structureManager= StructureManager.getInstance();
+	}
+	
+	/**
+	 * sets the new file path, creates a file and writes it
+	 * @param newFilePath the path for the file to be written
+	 */
+	public void write(String routingFilePath, Collection<NodeOfResource> finalRouting) {
+		
+		filePath= routingFilePath;
+		outputFile= new File(filePath);
+		try {
+			outputFileWriter= new BufferedWriter(new FileWriter(outputFile));
+		} catch (IOException e) {
+			System.err.println("Error occured trying to create a new file at path '" + filePath + "'");
+			e.printStackTrace();
+		}
+		writeAll(finalRouting);
+	}
+
+	
+	/**
+	 * writes the complete file, block by block until end of file (indicated by null)
+	 */
+	private void writeAll(Collection<NodeOfResource> finalRouting) {
+		
+		try {
+			writeHeader();
+			writeAllBlocks(finalRouting);
+		} catch (IOException e1) {
+			System.err.println("Error occured trying to write to buffered writer");
+			e1.printStackTrace();
+		}
+		
+		
+		try {
+			outputFileWriter.close();
+		} catch (IOException e) {
+			System.err.println("Error occured trying to create a new file: unable to close bufferedWriter");
+			e.printStackTrace();
+		}
+		
+	}
+	
 	protected void writeHeader() throws IOException {
 
 		outputFileWriter.write("Array size: " + parameterManager.X_GRID_SIZE + " x " + parameterManager.Y_GRID_SIZE + " logic blocks");
@@ -22,16 +99,15 @@ public class RoutingWriter extends AbstractWriter {
 		
 	}
 
-	@Override
-	protected void writeAllBlocks() throws IOException {
+	protected void writeAllBlocks(Collection<NodeOfResource> finalRouting) throws IOException {
 
 		for(Net n : structureManager.getNetCollection()) {
-			writeOneBlock(n);
+			writeOneBlock(n, finalRouting);
 		}
 		
 	}
 
-	private void writeOneBlock(Net n) throws IOException {
+	private void writeOneBlock(Net n, Collection<NodeOfResource> finalRouting) throws IOException {
 		
 		outputFileWriter.write("SOURCE (" + n.getSource().getX() + "," + n.getSource().getY() + ") "); //TODO fix red underlining 
 		if(n.getSource() instanceof LogicBlock) {
@@ -44,15 +120,8 @@ public class RoutingWriter extends AbstractWriter {
 				outputFileWriter.write("1");
 			} else outputFileWriter.write("0");
 		}
-		writePath(n.getSource());
 	}
 
-	private void writePath(PathElement currentPathElement) throws IOException {
-		if(!(currentPathElement instanceof OPin)) {
-			outputFileWriter.write(currentPathElement.forRoutingFile()); //TODO how is next for each channel saved, and how can we access them. Idea: maybe also create a variable for the class ChannelX and Y, so the access is easier
-		}else
-		
-	}
 
 	
 }
